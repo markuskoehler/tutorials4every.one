fd42ccef117491833162d6630eaa55d72f8b3e35
Markus Köhler <info@koehler.pro>
2017-11-05 21:20:56 +0100

    components for homepage

---
+++ resources/views/learning/_components/feedback.blade.php
@@ -0,0 +1,105 @@
+<div class="parallax cover overlay height-300 margin-none">
+    <img class="parallax-layer" data-auto-offset="true" data-auto-size="false"
+         src="{{asset('themes/learning/html/images/photodune-6745579-modern-creative-man-relaxing-on-workspace-m.jpg')}}" alt="Learning Cover"/>
+    <div class="parallax-layer overlay overlay-full overlay-bg-white bg-transparent" data-opacity="true"
+         data-speed="8">
+        <div class="v-center">
+            <div class="page-section">
+                <h1 class="text-display-2 overlay-bg-white margin-v-0-15 inline-block">Feedback</h1>
+                <br/>
+                <p class="lead text-overlay overlay-bg-white-strong inline-block">How others use E-learning to
+                    improve their skills</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="page-section">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="testimonial">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet doloremque
+                                enim error id, inventore magni odio odit quo tenetur.</p>
+                        </div>
+                    </div>
+                    <div class="media v-middle">
+                        <div class="media-left">
+                            <img src="{{asset('themes/learning/html/images/people/50/guy-8.jpg')}}" alt="People" class="img-circle width-40"/>
+                        </div>
+                        <div class="media-body">
+                            <p class="text-subhead margin-v-5-0">
+                                <strong>Adrian D. <span class="text-muted">@ Mosaicpro Inc.</span></strong>
+                            </p>
+                            <p class="small">
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star-o text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star-o text-yellow-800"></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="testimonial">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet doloremque
+                                enim error id, inventore magni odio odit quo tenetur.</p>
+                        </div>
+                    </div>
+                    <div class="media v-middle">
+                        <div class="media-left">
+                            <img src="{{asset('themes/learning/html/images/people/50/guy-6.jpg')}}" alt="People" class="img-circle width-40"/>
+                        </div>
+                        <div class="media-body">
+                            <p class="text-subhead margin-v-5-0">
+                                <strong>Adrian D. <span class="text-muted">@ Mosaicpro Inc.</span></strong>
+                            </p>
+                            <p class="small">
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star-o text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star-o text-yellow-800"></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="testimonial">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet doloremque
+                                enim error id, inventore magni odio odit quo tenetur.</p>
+                        </div>
+                    </div>
+                    <div class="media v-middle">
+                        <div class="media-left">
+                            <img src="{{asset('themes/learning/html/images/people/50/guy-3.jpg')}}" alt="People" class="img-circle width-40"/>
+                        </div>
+                        <div class="media-body">
+                            <p class="text-subhead margin-v-5-0">
+                                <strong>Adrian D. <span class="text-muted">@ Mosaicpro Inc.</span></strong>
+                            </p>
+                            <p class="small">
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star-o text-yellow-800"></span>
+                                <span class="fa fa-fw fa-star-o text-yellow-800"></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br/>
+
+</div>
